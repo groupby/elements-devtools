69464f7e7d8bfa969eecd1865d0a6157ec729668
ssbelle <shawna@Shawnas-MacBook-Pro.local>
2019-06-20 15:38:02 -0400

    Changed config

--- typedoc.js
+++ typedoc.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "SFX",
   "mode": "modules",
   "out": "./docs",
+  "readme": "none",
   "ignoreCompilerErrors": true,
   "exclude": ['**/test/**', '**/node_modules/**'],
 };
