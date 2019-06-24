48cb97351b92814f947ea24fddb5f822592fca02
ssbelle <shawna@Shawnas-MacBook-Pro.local>
2019-06-24 10:48:24 -0400

    Added config folder and moved tsdoc into config

---
+++ config/typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "name": "SF-X",
+  "mode": "modules",
+  "out": "./docs",
+  "readme": "none",
+  "ignoreCompilerErrors": true,
+  "exclude": ['**/test/**', '**/node_modules/**'],
+};
