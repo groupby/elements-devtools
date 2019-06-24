54d48b75c386d0d27fa9ce0c4a8dcc670a262c9b
ssbelle <shawna@Shawnas-MacBook-Pro.local>
2019-06-24 10:51:44 -0400

    Removed the typedoc.js from the root

--- typedoc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "name": "SF-X",
-  "mode": "modules",
-  "out": "./docs",
-  "readme": "none",
-  "ignoreCompilerErrors": true,
-  "exclude": ['**/test/**', '**/node_modules/**'],
-};
