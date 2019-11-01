b805c87c862d31e421afd1bf831769362e779891
DanielMoniz-GBI <46679515+DanielMoniz-GBI@users.noreply.github.com>
2019-11-01 14:22:39 -0400

    SFX-437: Update SF-X references to GB Elements (#5)
    Any references to `sfx`, `SF-X`, etc. are updated to account for branding as Elements. Most text references have been updated to `GB Elements`, with any important titles being called `GroupBy Elements`. Additionally, links to the View Layer and Logic Layer repositories have been updated.

--- config/typedoc.js
+++ config/typedoc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "name": "SF-X",
+  "name": "GroupBy Elements",
   "mode": "modules",
   "out": "./docs",
   // the readme: none setting exposes a list of all packages as the docs landing page instead of the top level readme
